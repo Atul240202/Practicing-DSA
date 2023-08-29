bdd61d8c6f8f64ed5136d9afedccfb53af89267e
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2023-08-29 21:38:00 +0530

    Create isMatrixSymmetric.cpp

---
+++ isMatrixSymmetric.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+bool isMatrixSymmetric(vector<vector<int>> matrix){
+    // Write your code here. 
+    int n = matrix.size();
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            if(matrix[i][j]!=matrix[j][i]) return false;
+        }
+    }
+    return true;
+}
